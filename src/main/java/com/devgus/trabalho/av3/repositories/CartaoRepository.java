dc31335a978548bc682432d40a9ccddabf5b345c
GustavoMoreira <gustavom.marques@hotmail.com>
2020-07-05 22:32:08 -0300

    Repositorios

---
+++ src/main/java/com/devgus/trabalho/av3/repositories/CartaoRepository.java
@@ -0,0 +1,10 @@
+package com.devgus.trabalho.av3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devgus.trabalho.av3.domain.Cartao;
+ 
+
+public interface CartaoRepository extends JpaRepository<Cartao, Integer>{
+
+}
---
+++ src/main/java/com/devgus/trabalho/av3/repositories/ContaRepository.java
@@ -0,0 +1,10 @@
+package com.devgus.trabalho.av3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devgus.trabalho.av3.domain.Conta;
+ 
+
+public interface ContaRepository extends JpaRepository<Conta, Integer>{
+
+}
